e5ea24442b5430d57312eb131d5d053eb1124ac6
BlisS <contacto@fixter.org>
2016-11-10 11:59:05 -0600

    eeror

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.contrib.auth import views as djangoViews
+from . import views
 
 
 urlpatterns = [
 	url(r'^login/$', djangoViews.login,name="login"),
 	url(r'^logout/$',djangoViews.logout,name="logout"),
+	url(r'^registration/$', views.Register.as_view(),name="registration")
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+from django.views.generic import View
 
-# Create your views here.
+
+
+
+class Register(View):
+	def get(self,request):
+		form = UserRegistrationForm()
+		template_name = 'account/nuevo_usuario.html'
+		context = {
+			'form':form
+		}
+		return render(request,template_name,context)
+
+	def post(self,request):
+		form = UserRegistrationForm(request.POST)
+		if form.is_valid():
+			new_user = form.save(commit=False)
+			new_user.set_password(form.cleaned_data['password'])
+			new_user.save()
+			return redirect('login')
+		template_name="account/nuevo_usuario.html"
+		context = {
+			'form':form
+		}
+		return render(request,template_name,context)
