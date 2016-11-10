807b030acd549078fb0bf3cb4056d49d677d581c
BlisS <contacto@fixter.org>
2016-11-10 11:30:52 -0600

    login logout

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from posts import urls as postsURLs
+from account import urls as accountURL
 
 
 urlpatterns = [
+	url(r'^account/',include(accountURL)),
     url(r'^admin/', admin.site.urls),
     url(r'^',include(postsURLs))
 ]
